47bd288c3e09ffd264fe54950cd6447b44266491
EmadBench <eb4579@nyu.edu>
2024-08-04 19:47:41 -0400

    Complete color flip project

--- contents/01-color-flipper/setup/hex.js
+++ contents/01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    color.textContent = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color.textContent += hex[getRandomNumber()];
+    }
+
+    document.body.style.backgroundColor = color.textContent;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
